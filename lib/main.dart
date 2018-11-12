74cd751c201e7b4441eef2eb7eaa158516d5ea94
Mohamed Elmi <maxto024@gmail.com>
2018-11-12 21:33:47 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutterJhipster/ui/login_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'flutterJhipster',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new LoginPage(),
+    );
+  }
+}
